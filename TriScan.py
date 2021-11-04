983a63ba209b104ed919c65697c250100f49249a
Alexis Lappe <LAPPEAA0646@UWEC.EDU>
2021-11-04 16:20:49 -0500

    Jinja

---
+++ TriScan.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+        return render_template("login.html")
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
+@app.route('/forgot_pass')
+def forgot_pass():
+    return render_template('forgot_pass.html')
+
+if __name__=="__main__":
+        app.run(debug=True)
