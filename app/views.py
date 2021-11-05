c01e171e1ce03f720a204bdeefbe6a4ff91761e8
Alexis Lappe <LAPPEAA0646@UWEC.EDU>
2021-11-05 12:56:51 -0500

    changes

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ def login():
     # if email is associate with student, render template student-index
     # if email is associate with faculty, render template faculty-index
 
-    return render_template("/student.html")
+    return render_template("/faculty.html")
 
 
 @app.route("/sign-up", methods=["GET", "POST"])
