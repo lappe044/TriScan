cdd156c3f092f5fbf7b2a9b4d9838c0559ed4e51
Michelle <chewpoim1308@uwec.edu>
2021-11-04 23:33:55 -0500

    created app module that contains files like __init__.py and views.py and a templates folder

---
+++ app/views.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+
+from app import app
+
+
+
+@app.route("/")
+def login():
+    
+    #email
+    #password
+
+
+    # if email is associate with student, render template student-index
+    # if email is associate with faculty, render template faculty-index
+
+    return render_template("/login.html")
+
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+
+        req = request.form
+
+        first_name = req["f_name"]
+        last_name = req["l_name"]
+        email = req["email"]
+        password = req["password"]
+        profession = req["profession"]
+
+        print(first_name,last_name,email,password, profession)
+
+        return redirect(request.url)
+
+    return render_template("/create_account.html")
+
+
+
+
+
