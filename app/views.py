7b47f5358e0b4062198c792ee99a628e30ce9d19
Michelle <chewpoim1308@uwec.edu>
2021-11-07 23:44:04 -0600

    modified views.py login.html create_account.html forgot_pass.html. All controllers are working properly

--- app/views.py
+++ app/views.py
@@ -3,16 +3,30 @@ from flask import Flask, render_template, request, redirect
 from app import app
 
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    
-    #email
-    #password
+
+    if request.method == "POST":
+
+        req = request.form
+
+        email = req["email"]
+        password = req["password"]
+
+        flag = True
+
+        # check email and password in database
+        if(flag):
+            redirect("/student")
+        else:
+            redirect("/faculty")
+
+        print(email, password)
 
 
-    # if email is associate with student, render template student-index
-    # if email is associate with faculty, render template faculty-index
+
+    # if email is associate with student, render template '/student'
+    # if email is associate with faculty, render template '/faculty'
 
     return render_template("/login.html")
 
@@ -31,11 +45,36 @@ def sign_up():
 
         print(first_name,last_name,email,password, profession)
 
-        return redirect(request.url)
+        redirect("/") # value is the route 
 
     return render_template("/create_account.html")
 
 
+@app.route("/forgot-password", methods=["GET", "POST"])
+def forgot_password():
+
+    if request.method == "POST":
+
+        req = request.form
+
+        email = req["email"]
+        new_password = req["new_pass"]
+
+        print(email,new_password)
+
+    # update the user's old password to new password
+
+        redirect('/')
+    
+    return render_template("/forgot_pass.html")
 
 
+@app.route("/student")
+def student_page():
 
+    return render_template("/student.html")
+
+@app.route("/faculty")
+def faculty_page():
+
+    return render_template("/faculty.html")
